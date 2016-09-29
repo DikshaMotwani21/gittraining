c0cec2324254e6df845fca37db5eccaa3e763152
DIKSHA MOTWANI <DIKSHA MOTWANI>
2016-09-29 23:37:55 +0530

    third commit

---
+++ Desktop/fe/frontend/resources/views/pages/fe3.blade.php
@@ -0,0 +1,160 @@
+<html><head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://pingendo.github.io/pingendo-bootstrap/themes/default/bootstrap.css" rel="stylesheet" type="text/css">
+  </head><body>
+    <div class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="page-header">
+              <h1>A BIG TITLE</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <ul class="nav nav-pills">
+              <li class="active">
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#"><i class="fa fa-star fa-fw"></i>Mobile</a>
+              </li>
+              <li>
+                <a href="#"><i class="fa fa-star fa-fw"></i>Tablets</a>
+              </li>
+              <li>
+                <a href="#"><i class="fa fa-star fa-fw"></i>laptops</a>
+              </li>
+              <li>
+                <a href="#"><i class="fa fa-star fa-fw"></i>Motherboards</a>
+              </li>
+              <li>
+                <a href="#"><i class="fa fa-star fa-fw"></i>Processors</a>
+              </li>
+              <li>
+                <a href="#"><i class="fa fa-star fa-fw"></i>Offers</a>
+              </li>
+              <li>
+                <a href="#"><i class="fa fa-star fa-fw"></i>Sale</a>
+              </li>
+              <li class="nav navbar-nav navbar-right">
+                <a href="#"><u>Login</u></a>
+              </li>
+              <li class="nav navbar-nav navbar-right">
+                <a href="#"><u>Sign Up</u></a>
+              </li>
+            </ul>
+            <a href="#">
+          </a>
+          </div>
+          <a href="#">
+        </a>
+        </div>
+        <a href="#">
+      </a>
+      </div>
+      <a href="#">
+    </a>
+    </div>
+    <a href="#">
+
+      
+    
+  
+
+</a>
+    <div class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-2">
+            <h3>Search Filters</h3>
+            <p>Search filters will be specific to the product Category type. These are
+              nothing but the attributes that are set in the backend while creating categories
+              and adding their attributes</p>
+          </div>
+          <div class="col-md-10">
+            <h3>Search results</h3>
+            <br>
+            <div class="dropdown">SORT
+              <select>
+                <option>Option One</option>
+                <option>Option Two</option>Prev 1 2 3 4 5 Next
+                <option>Option Three</option>
+              </select>
+              <ul class="pagination">
+                <li>
+                  <a href="#">Prev</a>
+                </li>
+                <li>
+                  <a href="#">1</a>
+                </li>
+                <li>
+                  <a href="#">2</a>
+                </li>
+                <li>
+                  <a href="#">3</a>
+                </li>
+                <li>
+                  <a href="#">4</a>
+                </li>
+                <li>
+                  <a href="#">5</a>
+                </li>
+                <li>
+                  <a href="#">Next</a>
+                </li>
+              </ul>
+              <div class="row">
+                <div class="col-md-3">
+                  <a><img src="https://unsplash.imgix.net/photo-1422222948315-28aadb7a2cb8?w=1024&amp;q=50&amp;fm=jpg&amp;s=cfeadbd7a991e58b553bee29a7eeca55" class="img-responsive img-thumbnail"></a>
+                </div>
+                <div class="col-md-3">
+                  <a><img src="https://unsplash.imgix.net/photo-1417128281290-30a42da46277?w=1024&amp;q=50&amp;fm=jpg&amp;s=2edb796eb5e657f2c82704a1e6573938" class="img-responsive"></a>
+                </div>
+                <div class="col-md-3">
+                  <a><img src="https://unsplash.imgix.net/photo-1423347834838-5162bb452ca7?w=1024&amp;q=50&amp;fm=jpg&amp;s=c255e589621f06513c1d123c7323fe9c" class="img-responsive"></a>
+                </div>
+                <div class="col-md-3">
+                  <a><img src="https://unsplash.imgix.net/photo-1422022098106-b3a9edc463af?w=1024&amp;q=50&amp;fm=jpg&amp;s=bbe0beb19ba846e0de37abd1f63acf13" class="img-responsive"></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    <div class="section">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <h3>Top Selling Products</h3>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-3">
+              <a><img src="https://unsplash.imgix.net/photo-1413920346627-a4389f0abd61?w=1024&amp;q=50&amp;fm=jpg&amp;s=cfd43f65c77577abb3bf29c0d6a0775f" class="img-responsive"></a>
+            </div>
+            <div class="col-md-3">
+              <a><img src="https://unsplash.imgix.net/reserve/fqUehZ3ZRru2Kh11qDBT_IMG_0042.jpg?w=1024&amp;q=50&amp;fm=jpg&amp;s=e96c5bfe57acbd6cdfdbe7d84f37656d" class="img-responsive"></a>
+            </div>
+            <div class="col-md-3">
+              <a><img src="https://ununsplash.imgix.net/photo-1414502622871-b90b0bec7b1f?w=1024&amp;q=50&amp;fm=jpg&amp;s=204c6064e8198b2ba44f367adf8ec819" class="img-responsive"></a>
+            </div>
+            <div class="col-md-3">
+              <a><img src="https://ununsplash.imgix.net/photo-1414637104192-f9ab9a0ee249?w=1024&amp;q=50&amp;fm=jpg&amp;s=39603686d9062f5c4a5309a45e28264e" class="img-responsive"></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  
+
+</body></html>
